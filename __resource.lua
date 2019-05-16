cda1a7b54ee0da36244a3a136ae7bb99505c0d8c
renildomarcio <krsolucoesweb@gmail.com>
2019-05-16 01:48:39 -0300

    Update #01
    Atuação em pt-br feito pelo usuario Super Gentil#0811

---
+++ __resource.lua
@@ -0,0 +1,25 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX Car Thief'
+
+version '1.0.0'
+
+server_scripts {
+    '@es_extended/locale.lua',
+	'config.lua',
+	'server/main.lua',
+  'locales/br.lua',
+	'locales/en.lua',
+	'locales/es.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'config.lua',
+	'client/main.lua',
+  'locales/br.lua',
+	'locales/en.lua',
+	'locales/es.lua'
+}
+
+dependency 'es_extended'
---
+++ locales/br.lua
@@ -0,0 +1,13 @@
+Locales['br'] = {
+  ['vehicle_robbery'] = 'roubo de veículo!',
+  ['mission_failed'] = 'missão fracassada!',
+  ['get_back_car_10s'] = 'você tem 10 segundos para voltar ao carro',
+  ['get_back_car_1m'] = 'você tem 1 minuto para voltar ao carro',
+  ['car_provided_rule'] = 'você tem que usar o carro que foi fornecido para você e você deve chegar a um ponto final.',
+  ['already_robbery'] = 'Já existe um roubo de carros em andamento!',
+  ['not_enough_cops'] = 'Policiais insuficientes na cidade!',
+  ['car_stealing_in_progress'] = 'carro roubando em andamento. O rastreador de veículos estará ativo no seu radar',
+  ['steal_a_car'] = 'pressione ~INPUT_CONTEXT~ roubar um carro',
+  ['drop_car_off'] = 'pressione ~INPUT_CONTEXT~ soltar o carro',
+  ['cooldown'] = 'um roubo de veículo foi concluído recentemente. Esperar %s segundos para roubar outro.'
+}
